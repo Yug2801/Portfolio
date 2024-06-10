e1fc2412d3cd9fd7edaa27d2b6b87c9b23913fe7
Yug2801 <95994453+Yug2801@users.noreply.github.com>
2024-06-10 14:02:24 +0530

    Components

---
+++ src/components/About.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Container, Col, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Fade from 'react-reveal';
+import Header from './Header';
+import endpoints from '../constants/endpoints';
+import FallbackSpinner from './FallbackSpinner';
+
+const styles = {
+  introTextContainer: {
+    margin: 10,
+    flexDirection: 'column',
+    whiteSpace: 'pre-wrap',
+    textAlign: 'left',
+    fontSize: '1.2em',
+    fontWeight: 500,
+  },
+  introImageContainer: {
+    margin: 0,
+    justifyContent: 'center',
+    display: 'flex',
+  },
+  images:
+  {
+    margin: '10%',
+    width: '55%',
+    height: '60%',
+  },
+};
+
+function About(props) {
+  const { header } = props;
+  const [data, setData] = useState(null);
+
+  const parseIntro = (text) => (
+    <ReactMarkdown
+      children={text}
+    />
+  );
+
+  useEffect(() => {
+    fetch(endpoints.about, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => err);
+  }, []);
+
+  return (
+    <>
+      <Header title={header} />
+      <div className="section-content-container">
+        <Container>
+          {data
+            ? (
+              <Fade>
+                <Row>
+                  <Col xs={12} sm={6} style={styles.introImageContainer}>
+                    <img src={data?.imageSource} style={styles.images} alt="profile" />
+                  </Col>
+                  <Col xs={12} sm={6} style={styles.introTextContainer}>
+                    {parseIntro(data.about)}
+                  </Col>
+                </Row>
+              </Fade>
+            )
+            : <FallbackSpinner />}
+        </Container>
+      </div>
+    </>
+  );
+}
+
+About.propTypes = {
+  header: PropTypes.string.isRequired,
+};
+
+export default About;
